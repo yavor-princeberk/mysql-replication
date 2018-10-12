b58f44421fcb443f04909c4e01609357ae96bf34
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-10-12 13:17:26 +0300

    Update setup-mysql-server.sh

--- setup-mysql-server.sh
+++ setup-mysql-server.sh
@@ -3,8 +3,10 @@ set -e
 if [ $MYSQL_MASTER ];
   then 
     cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf 
+    rm -f /var/lib/mysql/ib*
 fi
 if [ $MYSQL_SLAVE ]; 
   then 
    cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf 
+   rm -f /var/lib/mysql/ib* 
 fi 
