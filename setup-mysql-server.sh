a5577a2b46e4fecb676127cefa691b6b28fb9795
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-10-12 14:37:58 +0300

    Update setup-mysql-server.sh

--- setup-mysql-server.sh
+++ setup-mysql-server.sh
@@ -1,20 +1,20 @@
 #!/bin/bash 
 set -e
 if [ $MYSQL_MASTER ];
-  then 
-    cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf 
+  then
+    cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf
     rm -f /var/lib/mysql/ib*
     cd /var/lib/mysql/mysql
     rm -f innodb_* slave_*
-    /etc/init.d/mysql start 
-    mysql -u $MYSQL_MASTER_USER --password=MYSQL_MASTER_PASS mysql < /git-mysql/fixStatTables.sql
+    /etc/init.d/mysql start
+    mysql -u $MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD mysql < /git-mysql/fixStatTables.sql
 fi
-if [ $MYSQL_SLAVE ]; 
-  then 
-   cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf 
-   rm -f /var/lib/mysql/ib* 
+if [ $MYSQL_SLAVE ];
+  then
+   cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf
+   rm -f /var/lib/mysql/ib*
    cd /var/lib/mysql/mysql
    rm -f innodb_* slave_*
-   /etc/init.d/mysql start 
-   mysql -u $MYSQL_MASTER_USER --password=MYSQL_MASTER_PASS mysql < /git-mysql/fixStatTables.sql
-fi 
+   /etc/init.d/mysql start
+   mysql -u $MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD mysql < /git-mysql/fixStatTables.sql
+fi
