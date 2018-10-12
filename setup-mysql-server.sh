efc22bc982a30eb656217b9505745ab93946da09
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-10-12 14:20:42 +0300

    Update setup-mysql-server.sh

--- setup-mysql-server.sh
+++ setup-mysql-server.sh
@@ -4,10 +4,12 @@ if [ $MYSQL_MASTER ];
   then 
     cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf 
     rm -f /var/lib/mysql/ib*
+    cd /var/lib/mysql/mysql
+    rm -f innodb_* slave_*
+    /etc/init.d/mysql start 
 fi
 if [ $MYSQL_SLAVE ]; 
   then 
    cp /git-mysql/mysql-master.cnf /etc/mysql/my.cnf 
    rm -f /var/lib/mysql/ib* 
-   exec /git-mysql/insertUserIntoMaster.sh 
 fi 
