bbf6663507e6db90adcd9a15c8d8feff01b5df0e
yavor-princeberk <yavor@princeberk.com>
2018-10-18 12:59:24 +0000

    new changes

--- grantReplRights.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mysql -u $MYSQL_SLAVE_USER --password=$MYSQL_SLAVE_PASSWORD -e "grant replication on *.* 
-  to \'${MYSQL_SLAVE_REPL_USER}\'@\'${MYSQL_SLAVE_HOST}\' identified by \'${MYSQL_SLAVE_REPL_PASSWORD}\';"
