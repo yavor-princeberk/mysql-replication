745811dff20fbbd2bf47cb98d2442c81d9a35ac6
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-10-18 10:57:55 +0300

    Update confReplSlave.sh

--- confReplSlave.sh
+++ confReplSlave.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-cat mysql-slave.conf | sed 's/server_id*[0-9]$/$SLAVE_ID/g' > /etc/mysql/my.cnf 
+cat mysql-slave.cnf | sed -r "s/server-id\s*\=\s*[0-9]*/server-id\t\t= ${SERVER_ID}/g" > /etc/mysql/my.cnf
