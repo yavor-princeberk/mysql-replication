73304ecf969b079ddd026f281065512b183819c7
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-10-18 15:19:38 +0300

    Update grantReplRights.sh

--- grantReplRights.sh
+++ grantReplRights.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysql -u $MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD -e "grant replication on *.* 
+mysql -u $MYSQL_SLAVE_USER --password=$MYSQL_SLAVE_PASSWORD -e "grant replication on *.* 
   to \'${MYSQL_SLAVE_REPL_USER}\'@\'${MYSQL_SLAVE_HOST}\' identified by \'${MYSQL_SLAVE_REPL_PASSWORD}\';"
