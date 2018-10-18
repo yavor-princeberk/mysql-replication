cb445e502295c4ced538819f590afa7f5afdfad0
yavor-princeberk <yavor@princeberk.com>
2018-10-18 14:16:06 +0000

    new changes

--- setupLocalVariables.sh
+++ setupLocalVariables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-export MYSQL_ROOT_USER = "root" 
-export MYSQL_ROOT_PASSWORD = "1234qwer"
-export MYSQL_MASTER_HOST = "172.18.0.2"
-export MYSQL_SLAVE_HOST = "172.18.0.3"
-export MYSQL_REPL_USER = "repl"
-export MYSQL_REPL_PASSWORD = "repl"
+export MYSQL_ROOT_USER="root" 
+export MYSQL_ROOT_PASSWORD="1234qwer"
+export MYSQL_MASTER_HOST="172.18.0.2"
+export MYSQL_SLAVE_HOST="172.18.0.3"
+export MYSQL_REPL_USER="repl"
+export MYSQL_REPL_PASSWORD="repl"
 export MYSQL_MASTER_LOG_FILE="mysql-bin.000008"
 export MYSQL_MASTER_LOG_POSITION="120"
 
