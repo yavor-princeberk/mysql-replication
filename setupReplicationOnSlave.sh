8b21ac8d75207e30cf97520501998145532724e2
yavor-princeberk <yavor@princeberk.com>
2018-10-18 12:11:12 +0000

    adding new files

--- setupReplicationOnSlave.sh
+++ setupReplicationOnSlave.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mysql -u $MYSQL_SLAVE_USER --password=$MYSQL_SLAVE_PASSWORD -e "change master to master_host=\"$MYSQL_MASTER_HOST\", \ 
-  master_user=\"$MYSQL_MASTER_REPL_USER\", master_password=\"$MYSQL_MASTER_REPL_PASSWORD\", master_log_file=\"$MASTER_LOG_FILE\", \
-  master_log_pos=\"$MASTER_LOG_POS\";
+mysql -u $MYSQL_SLAVE_USER --password=$MYSQL_SLAVE_PASSWORD -e \
+"change master to master_host=\"${MYSQL_MASTER_HOST}\",  master_user=\"${MYSQL_SLAVE_REPL_USER}\", 
+master_password=\"${MYSQL_SLAVE_REPL_PASSWORD}\", master_log_file=\"${MYSQL_MASTER_LOG_FILE}\",  
+master_log_pos=${MYSQL_MASTER_LOG_POSITION};"
