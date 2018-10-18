183f60ba468876d4eb8fa7a7dbd2ba89a62adee1
yavor-princeberk <yavor@princeberk.com>
2018-10-18 13:17:35 +0000

    new changes

--- grantReplicationRightsToSlaveOnMaster.sh
+++ grantReplicationRightsToSlaveOnMaster.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mysql -u $MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD -e "grant replication on *.* 
-  to \'${MYSQL_REPL_USER}\'@\'${MYSQL_SLAVE_HOST}\' identified by \'${MYSQL_REPL_PASSWORD}\';"
+mysql -u $MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD -e "grant replication on *.* to '${MYSQL_REPL_USER}'@'${MYSQL_SLAVE_HOST}' identified by '${MYSQL_REPL_PASSWORD}';"
