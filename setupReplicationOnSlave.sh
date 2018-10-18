2b621b9e5f3e0282d586c52b199cfa9acea9cc29
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-10-18 11:10:15 +0300

    Update setupReplicationOnSlave.sh

--- setupReplicationOnSlave.sh
+++ setupReplicationOnSlave.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mysql -u $MYSQL_SLAVE_USER --password=$MYSQL_SLAVE_PASSWORD -e "change master to master_host='172.17.0.2', \ 
   master_user=\"$MYSQL_MASTER_REPL_USER\", master_password=\"$MYSQL_MASTER_REPL_PASSWORD\", master_log_file=\"$MASTER_LOG_FILE\", \
   master_log_pos=\"$MASTER_LOG_POS\";
