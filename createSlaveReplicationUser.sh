da41803e6a1886d9b56509d41a8b8ba4a6b3af37
yavor-princeberk <yavor@princeberk.com>
2018-10-20 16:41:59 +0000

    New changes

--- createSlaveReplicationUser.sh
+++ createSlaveReplicationUser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql -u $MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD -e "create user  '${MYSQL_SLAVE_REPL_USER}'@'${MYSQL_SLAVE_HOST}' identified by '${MYSQL_SLAVE_REPL_PASSWORD}';"
+mysql -u $MYSQL_ROOT_USER --password=$MYSQL_ROOT_PASSWORD -e "create user  '${MYSQL_SLAVE_REPL_USER}'@'${MYSQL_SLAVE_HOST}' identified by '${MYSQL_SLAVE_REPL_PASSWORD}';"
